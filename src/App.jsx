0eb18260b8f14b14501c589217993acf61089888
neutreNn <136928661+neutreNn@users.noreply.github.com>
2024-04-04 13:24:52 +0300

    git init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Main from "./Pages/Main/Main";
+import Header from "./components/Header/Header";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Main />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/helpers/formatDate.js
@@ -0,0 +1,9 @@
+﻿export const formatDate = (date) => {
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    }
+    return date.toLocaleDateString('en-US', options)
+}
