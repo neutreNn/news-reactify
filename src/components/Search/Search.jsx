69316d42def49301381fd5e54830a50a9b0eae15
neutreNn <136928661+neutreNn@users.noreply.github.com>
2024-04-13 14:14:02 +0300

    add search input and debounce hook

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,18 @@
+﻿
+import styles from './styles.module.css'
+
+const Search = ({keywords, setKeywords}) => {
+    return (
+        <div className={styles.search}>
+            <input 
+                type="text" 
+                value={keywords}
+                onChange={(e) => setKeywords(e.target.value)}
+                className={styles.input} 
+                placeholder="Search something..."
+            />
+        </div>
+    )
+}
+
+export default Search
