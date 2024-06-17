db366db192ba1c2c9be9f1a5ed679686ea8f82fa
neutreNn <136928661+neutreNn@users.noreply.github.com>
2024-06-17 13:10:55 +0300

    add news page and react router

---
+++ src/app/appRouter.tsx
@@ -0,0 +1,15 @@
+﻿import { createBrowserRouter }from "react-router-dom";
+import BaseLayout from "./layouts/BaseLayout";
+import { MainPage } from "@/Pages/main";
+import { NewsPage } from "@/Pages/news";
+
+export const appRouter = createBrowserRouter([
+  {
+    element: <BaseLayout />,
+    errorElement: <div>Error</div>,
+    children: [
+        {path: "/", element: <MainPage />}, 
+        {path: "/news/:id", element: <NewsPage />},
+    ]
+  }
+]);
--- src/entities/news/model/newsSlices.ts
+++ src/entities/news/model/newsSlices.ts
@@ -7,15 +7,17 @@ import { INews } from './types';
 export interface State {
   news: INews[];
   filters: IFilters;
+  currentNews: INews | null;
 }
 
 const initialState: State = {
   news: [],
+  currentNews: null,
   filters: {
     page_number: 1,
     page_size: PAGE_SIZE,
     category: null,
-    keywords: '',
+    keywords: '', 
   }
 }
 
@@ -30,9 +32,12 @@ export const newsSlice = createSlice({
       const { key, value } = action.payload;
       state.filters = { ...state.filters, [key]: value };
     },
+    setCurrentNews: (state, action: PayloadAction<INews | null>) => {
+      state.currentNews = action.payload;
+    },
   },
 });
 
-export const { setNews, setFilters } = newsSlice.actions;
+export const { setNews, setFilters, setCurrentNews } = newsSlice.actions;
 
 export default newsSlice.reducer;
